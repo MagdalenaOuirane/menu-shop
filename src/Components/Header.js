251f31e366b04a641e7175334bfc6d662ec2e435
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-01-27 23:56:22 +0100

    add map()

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,19 @@ import React from 'react';
 
 
 const Header = (props) => {
+
+
+    const activeItems = props.content.filter(item => item.active)
+
+
     return (
-        <header>Order summary</header>
+        <header>
+            <h1>Order quantity : {activeItems.length}</h1>
+            <h2>
+                To pay : {activeItems.length ? `${activeItems.length * 10} złotych` : ' dont buy, dont pay'}
+
+            </h2>
+        </header>
     )
 }
 
